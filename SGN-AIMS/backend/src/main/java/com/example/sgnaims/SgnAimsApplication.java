15596769dea072d351e4753a5ab51e797d582d68
Sun Jiayu <154002193+SunJiayu1019@users.noreply.github.com>
2026-05-27 08:07:54 +0800

    Add files via upload
    origin0526

---
+++ SGN-AIMS/backend/src/main/java/com/example/sgnaims/SgnAimsApplication.java
@@ -0,0 +1,13 @@
+package com.example.sgnaims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgnAimsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgnAimsApplication.class, args);
+	}
+
+}
---
+++ SGN-AIMS/backend/src/test/java/com/example/sgnaims/SgnAimsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sgnaims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgnAimsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
